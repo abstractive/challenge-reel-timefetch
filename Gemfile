3febb21d5c6cbc4f1cbab82e47080aaaf76cccb3
Dilum Navanjana <dilum@Dilums-MacBook-Pro.local>
2015-06-06 16:18:38 +0530

    Changes to time_zone

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'celluloid', :git => "git://github.com/celluloid/celluloid.git", :submodules => true , :branch => '0.17.0-prerelease'
+gem 'rspec-core'
+gem 'pry'
+gem 'http'
+gem 'reel'
--- time_zone.rb
+++ time_zone.rb
@@ -8,13 +8,11 @@ class FetchTimer
   include Celluloid
 
   def initialize
-    @url = "http://api.timezonedb.com/?zone="
+    @url = "http://api.timezonedb.com"
   end
 
   def get_time(time_zone)
-    @url << time_zone
-    @url << "&key=TYI8FA9XP77L&format=json"
-    response = HTTP.get(@url).to_s
+    response = HTTP.get(@url, :params => { :zone => time_zone, :key => "TYI8FA9XP77L", :format => "json" }).to_s
     hash = JSON[response]
 
     Time.at(hash['timestamp']).utc
@@ -23,24 +21,20 @@ class FetchTimer
 end
 
 zones = ['America/Los_Angeles', 'Asia/Colombo', 'Australia/Sydney', 'Africa/Johannesburg', 'Europe/Athens']
-results = []
 
 
+f = FetchTimer.new
 
 Reel::Server::HTTP.run('127.0.0.1', 2609) do |connection|
   connection.each_request do |request|
-
+    results = []
     zones.each do |element|
-      f = FetchTimer.new
       result = f.future.get_time(element)
       results << result
     end
 
-    my_hash = {:Los_Angeles => results[0].value, :Colombo => results[1].value, :Sydney => results[2].value, :Johannesburg => results[3].value, :Athens => results[4].value }
+    my_hash = {zones[0] => results[0].value, zones[1] => results[1].value, zones[2] => results[2].value, zones[3] => results[3].value, zones[4] => results[4].value }
     string = JSON.generate(my_hash)
-
     request.respond :ok , string
   end
 end
-
-
